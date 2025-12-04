7a09e775869ffa25925b67a6a4be83a7a3a4962b
Jonathan Moraes <jonathan.moraes38@gmail.com>
2025-12-03 21:50:01 -0300

    Add files via upload

---
+++ Quem_sou_eu.py
@@ -0,0 +1,10 @@
+nome = "Jonathan"
+sobre_nome= "Moraes"
+idade= 22
+curso= "Ciência da computação"
+
+print(f"""Me chamo {nome} {sobre_nome}, tenho {idade} anos, faço o curso de {curso} na UNIP,
+      meu objetivo é ingressar na área da programação, porém estou dando pequenos passos antes
+      de chegar nesse grande objetivo meu, o primeiro é focar em dominar Python, e para isso
+      irei adicionar projetos bem básicos para mostrar meu avanço com a linguagem. 
+      Se você leu até aqui, é um prazer te conhecer e obrigado por acompanhar :) """)
